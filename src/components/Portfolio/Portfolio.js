0ef077d676518248064ceb29884c1fd8098a622b
Nonkululeko <www.nonkululekontotoshe87@gmail.com>
2023-06-28 16:46:21 +0200

    url update

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -26,14 +26,16 @@ const Portfolio = () => {
       <span>Portfolio</span>
 
 
-     <Swiper style={{position: 'relative',
-width: '100%',
-height: '100%',
-zIndex: '1',
-display: 'flex',
-transitionProperty: 'transform',
-boxSizing: 'content-box',
-marginTop: '3rem'}}
+      <Swiper style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        zIndex: '1',
+        display: 'flex',
+        transitionProperty: 'transform',
+        boxSizing: 'content-box',
+        marginTop: '3rem'
+      }}
         className="portfolio-slider"
         navigation={true}
         loop={true}
@@ -43,45 +45,45 @@ marginTop: '3rem'}}
         loopFillGroupWithBlank={true}
       >
         <SwiperSlide>
-        <a href="https://example.com">
-          <img style= {{width: '20rem', height: '15rem'}} src={Login} alt="" />
+          <a href="https://example.com">
+            <img style={{ width: '20rem', height: '15rem' }} src={Login} alt="" />
           </a>
         </SwiperSlide>
 
         <SwiperSlide>
-        <a href="https://example.com">
-          <img style= {{width: '20rem', height: '15rem'}} src={Makeup} alt="" />
+          <a href="https://example.com">
+            <img style={{ width: '20rem', height: '15rem' }} src={Makeup} alt="" />
           </a>
         </SwiperSlide>
 
         <SwiperSlide>
-        <a href="file:///C:/Users/nonkululekon/Documents/My_Products.htm">
-          <img style= {{width: '20rem', height: '15rem'}} src={Products} alt="" />
+          <a href="file:///C:/Users/nonkululekon/Documents/My_Products.htm">
+            <img style={{ width: '20rem', height: '15rem' }} src={Products} alt="" />
           </a>
         </SwiperSlide>
 
         <SwiperSlide>
-        <a href="file:///C:/Users/nonkululekon/Documents/my_loginForm.htm">
-          <img style= {{width: '20rem', height: '15rem'}}src={MyLogin} alt="" />
+          <a href="file:///C:/Users/nonkululekon/Documents/my_loginForm.htm">
+            <img style={{ width: '20rem', height: '15rem' }} src={MyLogin} alt="" />
           </a>
         </SwiperSlide>
 
-       {/*  <SwiperSlide>
+        {/*  <SwiperSlide>
           <img style= {{width: '20rem', height: '15rem'}} src={Recipe} alt="" />
           </SwiperSlide> */}
 
-          <SwiperSlide>
-          <a href="file:///C:/Users/nonkululekon/Documents/Landing_Page.htm">
-          <img style= {{width: '20rem', height: '15rem'}} src={LandingPage} alt="" />
+        <SwiperSlide>
+          <a href="https://vermillion-gingersnap-ac8133.netlify.app/">
+            <img style={{ width: '20rem', height: '15rem' }} src={LandingPage} alt="" />
           </a>
         </SwiperSlide>
 
         <SwiperSlide>
-        <a href="file:///C:/Users/nonkululekon/Documents/My_recipes.htm">
-          <img style= {{width: '20rem', height: '15rem'}}src={Lotto} alt="" />
+          <a href="file:///C:/Users/nonkululekon/Documents/My_recipes.htm">
+            <img style={{ width: '20rem', height: '15rem' }} src={Lotto} alt="" />
           </a>
         </SwiperSlide>
-      </Swiper> 
+      </Swiper>
     </div>
   );
 };
@@ -92,7 +94,7 @@ export default Portfolio;
 
 
 
-   
-  
+
+
 
 
